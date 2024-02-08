b196a25655aed57a2e6035fbb4482f41b5df29db
khc <Administrator@304-5>
2024-02-08 14:10:15 +0900

    live-chat

---
+++ src/main/java/com/green/bloom/liveChat/ChatController.java
@@ -0,0 +1,30 @@
+package com.green.bloom.liveChat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ChatController {
+	
+
+	 	@GetMapping("/chat")
+	    public String chatGET(){
+
+	        log.info("@ChatController, chat GET()");
+	        
+	        return "chat";
+	    }
+	    @GetMapping("/live-chat")
+	    public String liveChat() {
+	    	return "liveChat/live-chat.html";
+	    }
+	    
+
+}
